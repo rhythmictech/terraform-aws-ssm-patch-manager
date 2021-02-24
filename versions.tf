b455d163ab2ba1e96da435bdea76a3f1f4f89860
Scott Miller <scott.miller171@gmail.com>
2021-02-24 17:36:42 -0500

    bump AWS provider required version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.20"
+      version = ">= 3.28" # v3.28.0 added significant improvements to plan-time validation for `ssm_maintenance_window_task`
     }
   }
 }
